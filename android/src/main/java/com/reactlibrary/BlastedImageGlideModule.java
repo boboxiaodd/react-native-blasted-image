5f48189a5a69f9801efc3ecfa096b62312fb4832
boboxiaodd <iam386@gmail.com>
2024-01-04 20:14:19 +0800

    Add files via upload

---
+++ android/src/main/java/com/reactlibrary/BlastedImageGlideModule.java
@@ -0,0 +1,8 @@
+package com.xerdnu.blastedimage;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public final class BlastedImageGlideModule extends AppGlideModule {
+}
